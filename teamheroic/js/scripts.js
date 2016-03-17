8596b8d731c9d674445bdd9227f0eef75f01a645
joshr9 <joshruchwarger@gmail.com>
2016-03-17 11:14:21 -0600

    added summary tab

--- teamheroic/js/scripts.js
+++ teamheroic/js/scripts.js
@@ -1,18 +1,20 @@
 (function() {
   var countTest = 0;
-  var app = angular.module('leaderboard', ['ngMaterial']);
+  var app = angular.module('leaderboard', ['ngMaterial', 'ngAnimate']);
   app.config(function($mdThemingProvider) {
     $mdThemingProvider.theme('dark')
-  
+
 
   })
-  app.controller('leaderboardController', ['$scope', '$http', '$interval', function($scope, $http, $interval) {
+  app.controller('leaderboardController', ['$scope', '$http', '$interval', '$timeout', function($scope, $http, $interval, $timeout) {
     $http.get('https://apis.trainheroic.com/public/leaderboard/468425')
       .then(function(data) {
-        //console.log(data)
+        console.log(data)
         $scope.people = []
         $scope.currentIndex = 0;
         var apiData = data.data.results
+        $scope.testTitle = data.data.tests[0].title
+        $scope.instructions = data.data.tests[0].testInstructions
 
         for (var i = 0; i < apiData.length; i++) {
 
@@ -22,26 +24,29 @@
             profileImg: apiData[i].profileImg,
             profileUrl: apiData[i].profileUrl,
             firstName: apiData[i].userFirstName,
-            lastName: apiData[i].userLastName
+            lastName: apiData[i].userLastName,
+            rx: apiData[i].tests[0]
           }
-
-
           $scope.people.push(person)
-
         }
 
         $scope.showPeople = []
         $scope.interval = 4
         $scope.tracker = 0
+
         $interval(function() {
-          if ($scope.interval <= $scope.people.length) {
-            console.log($scope.showPeople);
-            $scope.interval += 4
-            $scope.tracker += 4
-          } else {
-            $scope.interval = 4
-            $scope.tracker = 0
-          }
+          $scope.transition = true
+          $timeout(function() {
+            if ($scope.interval <= $scope.people.length) {
+              $scope.interval += 4
+              $scope.tracker += 4
+            } else {
+              $scope.interval = 4
+              $scope.tracker = 0
+            }
+            $scope.transition = false
+          }, 500);
+
         }, 5000)
       })
   }])
